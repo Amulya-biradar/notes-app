1af3493aa064b498822912759404b1794648bb46
Amulya-biradar <amulyacb14@gmail.com>
2026-05-22 16:01:03 +0530

    Implement service layer

---
+++ notes-backend/src/main/java/com/notesapp/notes_backend/service/NoteService.java
@@ -0,0 +1,13 @@
+package com.notesapp.notes_backend.service;
+
+import com.notesapp.notes_backend.dto.request.NoteRequestDto;
+import com.notesapp.notes_backend.dto.response.NoteResponseDto;
+
+import java.util.List;
+
+public interface NoteService {
+
+    NoteResponseDto createNote(NoteRequestDto dto);
+
+    List<NoteResponseDto> getAllNotes();
+}
---
+++ notes-backend/src/main/java/com/notesapp/notes_backend/service/impl/NoteServiceImpl.java
@@ -0,0 +1,39 @@
+package com.notesapp.notes_backend.service.impl;
+
+import com.notesapp.notes_backend.dto.request.NoteRequestDto;
+import com.notesapp.notes_backend.dto.response.NoteResponseDto;
+import com.notesapp.notes_backend.entity.Note;
+import com.notesapp.notes_backend.mapper.NoteMapper;
+import com.notesapp.notes_backend.repository.NoteRepository;
+import com.notesapp.notes_backend.service.NoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NoteServiceImpl implements NoteService {
+
+    private final NoteRepository noteRepository;
+
+    @Override
+    public NoteResponseDto createNote(NoteRequestDto dto) {
+
+        Note note = NoteMapper.toEntity(dto);
+
+        Note savedNote = noteRepository.save(note);
+
+        return NoteMapper.toResponseDto(savedNote);
+    }
+
+    @Override
+    public List<NoteResponseDto> getAllNotes() {
+
+        List<Note> notes = noteRepository.findAll();
+
+        return notes.stream()
+                .map(NoteMapper::toResponseDto)
+                .toList();
+    }
+}
