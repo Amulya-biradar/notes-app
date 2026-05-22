75ba3d844301660a16d496786f67c38f1e31cdc4
Amulya-biradar <amulyacb14@gmail.com>
2026-05-22 16:09:07 +0530

    Implemented create and get notes APIs

---
+++ notes-backend/src/main/java/com/notesapp/notes_backend/controller/NoteController.java
@@ -0,0 +1,29 @@
+package com.notesapp.notes_backend.controller;
+
+import com.notesapp.notes_backend.dto.request.NoteRequestDto;
+import com.notesapp.notes_backend.dto.response.NoteResponseDto;
+import com.notesapp.notes_backend.service.NoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notes")
+@RequiredArgsConstructor
+public class NoteController {
+
+    private final NoteService noteService;
+
+    @PostMapping
+    public NoteResponseDto createNote(@RequestBody NoteRequestDto dto) {
+
+        return noteService.createNote(dto);
+    }
+
+    @GetMapping
+    public List<NoteResponseDto> getAllNotes() {
+
+        return noteService.getAllNotes();
+    }
+}
